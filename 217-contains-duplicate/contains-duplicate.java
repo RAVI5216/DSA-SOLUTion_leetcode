e9807ae6ee6423f484d814f1f6ed9b571e748997
Ravi Kumar <ravikumar117284@gmail.com>
2026-09-26 11:16:23 +0530

    Time: 20 ms (38.79%) | Memory: 108.2 MB (38.02%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -3,7 +3,7 @@ class Solution {
         HashSet<Integer> set=new HashSet<>();
         for(int num:nums)
         {
-            if(set.contains(num))
+            if(!set.add(num))
             {
                 return true;
             }
